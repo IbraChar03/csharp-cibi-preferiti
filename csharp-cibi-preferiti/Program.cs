61ac9862f0c212f9e8edc9f0755b8185866aae7f
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-04 14:24:26 +0200

    esercizio cibi

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string[] cibiPreferiti = { "pizza","kebab", "pasta al ragù","lasagna", "pasta al pesto" };
+
+Console.WriteLine($"La classifica è formata da {cibiPreferiti.Length} cibi");
+Console.WriteLine($"Classifica:");
+var n = 0;
+
+foreach (var item in cibiPreferiti)
+{
+    Console.WriteLine($" {n + 1} posto : {item}");
+    n++;
+}
+
+Console.WriteLine($" Il cibo in primo posto è : {cibiPreferiti.AsQueryable().First()}");
+Console.WriteLine($" Il cibo all`ultimo posto  è : {cibiPreferiti.AsQueryable().Last()}");
+Console.WriteLine($" Il cibo di mezza classifica  è : {cibiPreferiti[cibiPreferiti.Length / 2]}");
+
