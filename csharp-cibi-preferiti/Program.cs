b447e9e915299123e8f760df5d8a9a75f23e2422
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-04 14:36:41 +0200

    fine bonus

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string[] cibiPreferiti = { "pizza","kebab", "pasta al ragù","lasagna", "pasta al pesto" };
+string[] cibiPreferiti = { "pizza","kebab", "pasta al ragù","lasagna", "pasta al pesto" ,"carbonara"};
 
 Console.WriteLine($"La classifica è formata da {cibiPreferiti.Length} cibi");
 Console.WriteLine($"Classifica:");
@@ -13,5 +13,9 @@ foreach (var item in cibiPreferiti)
 
 Console.WriteLine($" Il cibo in primo posto è : {cibiPreferiti.AsQueryable().First()}");
 Console.WriteLine($" Il cibo all`ultimo posto  è : {cibiPreferiti.AsQueryable().Last()}");
+if (cibiPreferiti.Length % 2 == 0)
+
+    Console.WriteLine($" I cibi di mezza classifica  sono : {cibiPreferiti[cibiPreferiti.Length / 2 - 1]} e {cibiPreferiti[cibiPreferiti.Length / 2]}");
+else
 Console.WriteLine($" Il cibo di mezza classifica  è : {cibiPreferiti[cibiPreferiti.Length / 2]}");
 
